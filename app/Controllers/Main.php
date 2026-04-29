2f9cd844c55001ff6a8e19e76d0718cc2483f12e
shyestfern <zalesak_maxmilian@oauh.cz>
2026-04-29 08:43:17 +0200

    Přidání modelu RaceYear do controlleru Main

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -7,16 +7,27 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
+use App\Models\RaceYear;
+
 class Main extends BaseController
 {
+    public $data;
+    public $raceyear;
 
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         parent::initController($request, $response, $logger);
+
+        $this->raceyear = new RaceYear();
+        $dataRaceYear = $this->raceyear->findAll();
+
+        $this->data = [
+            "raceyear" => $dataRaceYear
+        ];
     }
 
     public function index()
     {
-        
+        echo view("index", $this->data);
     }
 }
