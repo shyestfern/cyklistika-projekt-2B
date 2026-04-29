e8101f0444976ecb5d9186a26abe121909574ed0
shyestfern <zalesak_maxmilian@oauh.cz>
2026-04-29 09:31:26 +0200

    Rozpracování úvodní stránky

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -7,27 +7,37 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
+use App\Models\Race;
 use App\Models\RaceYear;
 
 class Main extends BaseController
 {
     public $data;
-    public $raceyear;
+    public $race;
 
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         parent::initController($request, $response, $logger);
 
-        $this->raceyear = new RaceYear();
-        $dataRaceYear = $this->raceyear->findAll();
+        $this->race = new Race();
+        $dataRace = $this->race->findAll();
 
         $this->data = [
-            "raceyear" => $dataRaceYear
+            "race" => $dataRace
         ];
     }
 
     public function index()
     {
+        $dataZavodu = $this->race->join("race_year", "race.default_name = race_year.real_name", "inner")
+            ->where("race_year.category", "E")
+            ->where("race_year.sex", "M")
+            ->findAll();
+        
+        $this->data += [
+            "zavod" => $dataZavodu
+        ];
+        
         echo view("index", $this->data);
     }
 }
