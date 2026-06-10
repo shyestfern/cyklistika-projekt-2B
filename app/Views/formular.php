4dc4733fc4ecaa5aa6c21d80d1bcd3be7083ef76
shyestfern <zalesak_maxmilian@oauh.cz>
2026-06-10 08:20:41 +0200

    Úprava formuláře pomocí form helperu

--- app/Views/formular.php
+++ app/Views/formular.php
@@ -2,14 +2,23 @@
 
 <?= $this->section("content"); ?>
 
-
-
 <?= form_open('polozka/vytvorit') ?>
+
     <div class="form-floating mt-2 mb-2">
-        <input name="real_name" class="form-control" placeholder="Vložte název prvku">
+        
+    <?php
+        $data = array(
+            'name' => 'real_name',
+            'class' => 'form-control',
+            'placeholder' => 'Vložte název prvku'
+        );
+
+        echo form_input($data);
+    ?>
+
         <label for="real_name" class="form-label">real name</label>
     </div>
     <button type="submit" class="btn btn-primary">Odeslat</button>
-</form>
+<?= form_close() ?>
 
 <?= $this->endSection(); ?>
