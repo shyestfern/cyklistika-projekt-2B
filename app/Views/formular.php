d12d3441964b87a91d42ef55603579f8076673fa
shyestfern <zalesak_maxmilian@oauh.cz>
2026-06-10 09:11:46 +0200

    Zhotovení struktury kódu formuláře

--- app/Views/formular.php
+++ app/Views/formular.php
@@ -2,23 +2,38 @@
 
 <?= $this->section("content"); ?>
 
-<?= form_open('polozka/vytvorit') ?>
+<?php
 
-    <div class="form-floating mt-2 mb-2">
-        
-    <?php
-        $data = array(
-            'name' => 'real_name',
-            'class' => 'form-control',
-            'placeholder' => 'Vložte název prvku'
-        );
+    $dataInput = array(
+        'name' => 'real_name',
+        'id' => 'name',
+        'class' => 'form-control',
+        'placeholder' => 'Vložte název prvku'
+    );
 
-        echo form_input($data);
-    ?>
+    $dataLabel = array(
+        'class' => 'form-label'
+    );
 
-        <label for="real_name" class="form-label">real name</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Odeslat</button>
+    $dataBtn = array(
+        'type' => 'submit',
+        'class' => 'btn btn-primary',
+        'content' => 'Odeslat'
+    );
+
+    echo form_open('polozka/vytvorit');
+
+?>
+
+<div class="form-floating mt-2 mb-2">
+
+    <?= form_input($dataInput); ?>
+    <?= form_label('real name', 'name', $dataLabel); ?>
+
+</div>
+
+<?= form_button($dataBtn); ?>
+
 <?= form_close() ?>
 
 <?= $this->endSection(); ?>
